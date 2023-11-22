0a0d4ecd922fe24810b9416ce8e101ed546e764d
Sara-Bader <sahrabader2@gmail.com>
2023-11-22 20:33:05 +0200

    e

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,20 +1,22 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-  
-    const currentHour = new Date().getHours();
-  
-    const welcomeMessage = document.getElementById('welcome-message');
-  
+  const currentHour = new Date().getHours();
+  const welcomeMessage = document.getElementById('welcome-message');
+  const loaderWrapper = document.querySelector('.loader-wrapper');
 
-    let greeting;
-    if (currentHour >= 5 && currentHour < 12) {
+  let greeting;
+  if (currentHour >= 5 && currentHour < 12) {
       greeting = 'Good morning';
-    } else if (currentHour >= 12 && currentHour < 18) {
+  } else if (currentHour >= 12 && currentHour < 18) {
       greeting = 'Good afternoon';
-    } else {
+  } else {
       greeting = 'Good evening';
-    }
-  
-    welcomeMessage.textContent = `${greeting}`;
+  }
+
+  welcomeMessage.textContent = `${greeting}`;
+
+  loaderWrapper.style.display = 'flex';
+
+  window.addEventListener('load', function () {
+      loaderWrapper.style.display = 'none';
   });
-  
+});
